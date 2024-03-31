f36104f4771154793078c7e453f5c8a7967ed93d
Valentino Olmedo <valenolme@gmail.com>
2024-03-31 10:40:03 -0300

    segundo commit

--- tp1_1.cpp
+++ tp1_1.cpp
@@ -1,5 +1,14 @@
 #include <stdio.h>
 
 int main(){
+    int num, *pnum;
     printf("Hola mundo");
+    num=1;
+    pnum=&num;
+    printf("\n%d",*pnum);
+    printf("\n%p",pnum);
+    printf("\n%p",&num);
+    printf("\n%p",&pnum);
+    printf("\n%d",sizeof(num));
+    return 0;
 }
